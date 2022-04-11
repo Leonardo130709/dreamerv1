shift in .learn() removed, entropy sign fixed, retain_graph inserted

src/agent.py
@@ -5,21 +5,21 @@ from itertools import chain
 from torch.nn.utils import clip_grad_norm_
 from pytorch3d.loss import chamfer_distance
 import pdb
+
 nn = torch.nn
 F = nn.functional
 td = torch.distributions
+torch.autograd.set_detect_anomaly(True)
 
 
 # todo check if burn_in and storing hidden_states will be useful
 
+
 class Agent(nn.Module):
-    def __init__(self, obs_dim, act_dim, encoder, decoder, callback,
-                 config):
+    def __init__(self, obs_dim, act_dim, callback, config):
         super().__init__()
-        self.c = config
+        self._c = config
         self.obs_dim, self.act_dim = obs_dim, act_dim
-        self.encoder = encoder
-        self.decoder = decoder or nn.Linear(self.c.deter_dim + self.c.stoch_dim, obs_dim)
         self.callback = callback
         self._step = 0
         self._build()
@@ -39,7 +39,7 @@ class Agent(nn.Module):
         dist = self.actor(feat)
         if training:
             action = dist.sample()
-            action = action + self.c.expl_scale*torch.randn_like(action)
+            action = action + self._c.expl_scale * torch.randn_like(action)
         else:
             action = dist.sample([100]).mean(0)
         action = torch.clamp(action, -1, 1)
@@ -50,29 +50,23 @@ class Agent(nn.Module):
         #   gumbel, target_ntwotrk, reinforce gradients, entropy, layer norm
         self._model_params.requires_grad_(True)
         observations_emb = self.encoder(observations)
-        observations, observations_emb, actions, rewards = map(lambda t: t[1:],
-                                                       (observations, observations_emb, actions.roll(1, 0), rewards))
 
-        #next_observations, actions, rewards = map(lambda t: t[:-1], (next_observations, actions, rewards))
         posts, priors = self.wm.observe(observations_emb, actions, states[0])
         feat = self.wm.get_feat(posts)
         obs_pred = self.decoder(feat)
         reward_pred = self.reward_model(feat)
-        prior_dist = self.wm.get_dist(priors)
-        post_dist = self.wm.get_dist(posts)
-        fixed_prior_dist = self.wm.get_dist(priors.detach())
-        fixed_post_dist = self.wm.get_dist(posts.detach())
-        div = self.c.alpha*td.kl_divergence(fixed_post_dist, prior_dist) + \
-              (1. - self.c.alpha)*td.kl_divergence(post_dist, fixed_prior_dist)
+        post_dist, fixed_post_dist, prior_dist, fixed_prior_dist = map(self.wm.get_dist,
+                                                                       (posts, posts.detach(), priors, priors.detach()))
+        div = self._c.alpha * td.kl_divergence(fixed_post_dist, prior_dist) + \
+              (1. - self._c.alpha) * td.kl_divergence(post_dist, fixed_prior_dist)
         div = div.mean()
-        #div = torch.maximum(div, torch.full_like(div, self.c.free_nats))
 
-        if self.c.encoder == 'PointNet':
+        if self._c.observe == 'point_cloud':
             obs_loss = chamfer_distance(obs_pred.flatten(0, 1), observations.flatten(0, 1))[0].mean()
         else:
             obs_loss = (obs_pred - observations).pow(2).mean()
         reward_loss = (reward_pred - rewards).pow(2).mean()
-        model_loss = obs_loss + reward_loss + self.c.kl_scale * div
+        model_loss = obs_loss + reward_loss + self._c.kl_scale * div
         self.callback.add_scalar('train/obs_loss', obs_loss, self._step)
         self.callback.add_scalar('train/reward_loss', reward_loss, self._step)
         self.callback.add_scalar('train/kl_div', div, self._step)
@@ -80,27 +74,29 @@ class Agent(nn.Module):
         self.callback.add_scalar('train/mean_rewards', rewards.mean(), self._step)
         self.callback.add_scalar('train/model_grads', utils.grads_sum(self._model_params), self._step)
 
-        self._wm_optim.zero_grad()
-        model_loss.backward()
-        #clip_grad_norm_(self._model_params, self.c.max_grad)
+        self.wm_optim.zero_grad()
+        # todo: careful with retain_graph
+        model_loss.backward(retain_graph=True)
+        clip_grad_norm_(self._model_params, self._c.max_grad)
         self._model_params.requires_grad_(False)
 
-        imag_feat, actions = self.imagine_ahead(posts.detach())
+        imag_feat, actions = self.imagine_ahead(posts.detach())  # could try with priors
         rewards = self.reward_model(imag_feat)
         values = self._target_critic(imag_feat)
-        target_values = utils.gve2(rewards, values, self.c.discount, self.c.disclam)
+        target_values = utils.gve2(rewards, values, self._c.discount, self._c.disclam)
         values, imag_feat, actions = map(lambda t: t[:-1], (values, imag_feat, actions))
         dist = self.actor(imag_feat)
         log_prob = dist.log_prob(actions).unsqueeze(-1)
         ent = -log_prob
-        #reinforce_loss = log_probs*(target_values - values).detach()
+        # reinforce_loss = -log_probs*(target_values - values).detach()
         reinforce_loss = 0
-        actor_gain = self.c.rho*reinforce_loss + (1-self.c.rho)*target_values - self.c.eta*ent
+        actor_gain = -self._c.rho * reinforce_loss + (1 - self._c.rho) * target_values + self._c.eta * ent
         discount = self.masked_discount(target_values, 0)
-        actor_loss = - torch.mean(discount*actor_gain)
+        actor_loss = - torch.mean(discount * actor_gain)
 
-        self._actor_optim.zero_grad()
+        self.actor_optim.zero_grad()
         actor_loss.backward()
+        clip_grad_norm_(self.actor.parameters(), self._c.max_grad)
         self.callback.add_scalar('train/actor_ent', ent.mean(), self._step)
         self.callback.add_scalar('train/actor_loss', actor_gain.mean(), self._step)
         self.callback.add_scalar('train/mean_value', values.mean(), self._step)
@@ -108,19 +104,17 @@ class Agent(nn.Module):
 
         values = self.critic(imag_feat.detach())
         critic_loss = (discount * (values - target_values.detach()).pow(2)).mean()
-        self._critic_optim.zero_grad()
+        self.critic_optim.zero_grad()
         critic_loss.backward()
+        clip_grad_norm_(self.critic.parameters(), self._c.max_grad)
         self.callback.add_scalar('train/critic_loss', critic_loss, self._step)
         self.callback.add_scalar('train/critic_grads', utils.grads_sum(self.critic), self._step)
 
-        self._wm_optim.step()
-        self._actor_optim.step()
-        self._critic_optim.step()
+        self.wm_optim.step()
+        self.actor_optim.step()
+        self.critic_optim.step()
+        utils.soft_update(self._target_critic, self.critic, self._c.critic_polyak)
         self._step += 1
-
-        if self._step % self.c.target_update == 0:
-            for pt, po in zip(self._target_critic.parameters(), self.critic.parameters()):
-                pt.copy_(po)
 
         return model_loss, actor_loss, critic_loss
 
@@ -131,8 +125,9 @@ class Agent(nn.Module):
             return dist.rsample()
 
         state = posts[:-1].reshape(-1, posts.size(-1))
+        # cause last could be terminal state
         states, actions = [], []
-        for _ in range(self.c.horizon):
+        for _ in range(self._c.horizon):
             action = policy(state)
             states.append(state)
             actions.append(action)
@@ -145,26 +140,43 @@ class Agent(nn.Module):
     def masked_discount(self, x, mask_size):
         mask = torch.cat([torch.zeros(mask_size, device=self.device),
                           torch.ones(x.size(0) - mask_size, device=self.device)])
-        discount = self.c.discount**torch.arange(0, x.size(0), device=self.device)
-        discount = discount*mask
-        shape = (x.ndimension()-1)*(1,)
-        return discount.view(-1, *shape)
+        discount = self._c.discount ** torch.arange(0, x.size(0), device=self.device)
+        discount = discount * mask
+        shape = (x.ndimension() - 1) * (1,)
+        return discount.reshape(-1, *shape)
 
     def _build(self):
-        feat = self.c.deter_dim + self.c.stoch_dim
-        self.actor = models.NormalActor(feat, self.act_dim, self.c.actor_layers)
+        feat = self._c.deter_dim + self._c.stoch_dim
+        self.actor = models.NormalActor(feat, self.act_dim, self._c.actor_layers)
         # self.actor = models.OrdinalActor(feat, self.act_dim, utils.Spec(-1, 1, 10))
-        self.critic = utils.build_mlp([feat] + self.c.critic_layers + [1])
+        self.critic = utils.build_mlp(feat, *self._c.critic_layers, 1)
         self._target_critic = deepcopy(self.critic).requires_grad_(False)
-        self.reward_model = utils.build_mlp([feat] + self.c.critic_layers + [1])
-        self.wm = models.RSSM(self.obs_dim, self.act_dim, self.c.deter_dim, self.c.stoch_dim, self.c.wm_layers)
-        # self._target_actor, self._target_critic = map(lambda m: deepcopy(m).requires_grad_(False),
-        #                                               (self.actor, self.critic))
+        self.reward_model = utils.build_mlp(feat, *self._c.wm_layers, 1)
+        self.wm = models.RSSM(self._c.obs_emb_dim, self.act_dim, self._c.deter_dim, self._c.stoch_dim,
+                              self._c.wm_layers)
+        if self._c.observe == 'states':
+            self.encoder = nn.Sequential(
+                nn.Linear(self.obs_dim, self._c.obs_emb_dim),
+                nn.ELU(),
+                nn.Linear(self._c.obs_emb_dim, self._c.obs_emb_dim),
+                nn.Tanh(),
+            )
+            self.decoder = nn.Linear(feat, self.obs_dim)
+        elif self._c.observe == 'pixels':
+            self.encoder = models.PixelEncoder(3, self._c.obs_emb_dim)
+            self.decoder = models.PixelDecoder(feat, 3)
+        elif self._c.observe == 'point_cloud':
+            self.encoder = models.PointCloudEncoder(3, self._c.obs_emb_dim,
+                                                    layers=self._c.pn_layers, dropout=self._c.pn_dropout)
+            self.decoder = models.PointCloudDecoder(feat, 3, self._c.pn_number)
+        else:
+            raise NotImplementedError
+
         self._model_params = nn.ParameterList(
             chain(*map(nn.Module.parameters, (self.encoder, self.wm, self.decoder, self.reward_model)))
         )
-        self._actor_optim = torch.optim.Adam(self.actor.parameters(), self.c.actor_lr)
-        self._critic_optim = torch.optim.Adam(self.critic.parameters(), self.c.critic_lr)
-        self._wm_optim = torch.optim.Adam(self._model_params, self.c.wm_lr)
-        self.device = torch.device(self.c.device if torch.cuda.is_available() else 'cpu')
+        self.actor_optim = torch.optim.Adam(self.actor.parameters(), self._c.actor_lr)
+        self.critic_optim = torch.optim.Adam(self.critic.parameters(), self._c.critic_lr)
+        self.wm_optim = torch.optim.Adam(self._model_params, self._c.wm_lr)
+        self.device = torch.device(self._c.device if torch.cuda.is_available() else 'cpu')
         self.to(self.device)

src/core.py
@@ -1,104 +1,113 @@
+import pathlib
 from dataclasses import dataclass
 from .agent import Agent
 from . import utils, wrappers
-import gym
 import torch
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
-from statistics import mean
-from dm_control import suite
-import datetime
+import numpy as np
 import pdb
 
 torch.autograd.set_detect_anomaly(True)
 
+
 @dataclass
-class Config:
+class Config(utils.AbstractConfig):
     #task
-    discount = .99
-    disclam = .95
-    horizon = 15
-    kl_scale = 1.
-    expl_scale = .5
-    alpha = .8
-    rho = 0.
-    eta = 1e-4
+    discount: float = .99
+    disclam: float = .95
+    horizon: int = 15
+    kl_scale: float = 1.
+    expl_scale: float = .5
+    alpha: float = .8
+    rho: float = 0.
+    eta: float = 1e-4
     action_repeat = 2
 
     #model
-    actor_layers = 2*[128]
-    critic_layers = 3*[200]
-    wm_layers = 2*[200]
-    deter_dim = 200
-    stoch_dim = 32
-    emb_dim = 32
+    actor_layers: tuple = (256, 256)
+    critic_layers: tuple = (256, 256)
+    wm_layers: tuple = (200, 200)
+    deter_dim: int = 164
+    stoch_dim: int = 24
+    obs_emb_dim: int = 32
 
-    pn_layers = 2
-    pn_depth = 32
-    pn_number = 800
+    pn_layers: tuple = (64, 128)
+    pn_depth: int = 32
+    pn_number: int = 600
 
     #train
-    actor_lr = 8e-5
-    critic_lr = 8e-5
-    wm_lr = 6e-4
-    batch_size = 50
-    max_grad = 100
-    seq_len = 50
-    buffer_capacity = 10**2
-    training_steps = 300
-    target_update = 99
+    actor_lr: float = 8e-5
+    critic_lr: float = 8e-5
+    critic_polyak: float = .99
+    wm_lr: float = 6e-4
+    batch_size: int = 40
+    max_grad: float = 100.
+    seq_len: int = 50
+    buffer_capacity: int = 3*10**2
+    training_steps: int = 400
+    eval_freq: int = 10000
 
-    device = 'cuda'
-    encoder = 'PointNet'
-    task = 'walker_stand'
+    device: str = 'cuda'
+    observe: str = 'states'
+    task: str = 'walker_stand'
+    logdir: str = 'dreamer_logdir'
 
 
 class Dreamer:
     def __init__(self, config):
-        self.c = config
-        obs_dim, act_dim, enc_obs_dim = self._make_env()
-        self.callback = SummaryWriter(log_dir=f'./dreamer_logdir/{config.encoder}/{config.task}/{datetime.datetime.now()}')
-        self.agent = Agent(enc_obs_dim, act_dim, self.encoder, self.decoder, self.callback, config)
+        self.config = config
+        obs_dim, act_dim = self._make_env()
+        self._task_path = pathlib.Path('.').joinpath(f'./{config.logdir}/{config.task}/{config.observe}')
+        self.callback = SummaryWriter(log_dir=self._task_path)
+        self.agent = Agent(obs_dim, act_dim, self.callback, config)
         self.buffer = utils.TrajectoryBuffer(config.buffer_capacity, config.seq_len)
+        self.interactions_count = 0
 
     def learn(self):
+        self.config.save(self._task_path / 'config')
+
         def policy(obs, state, training):
             if not torch.is_tensor(obs):
                 obs = torch.from_numpy(obs[None]).to(self.agent.device)
             action, state = self.agent.act(obs, state, training)
             return action.detach().cpu().flatten().numpy(), state
-        t = 0
+        
         while True:
             tr = utils.simulate(self.env, policy, True)
+            self.interactions_count += 1000
             self.buffer.add(tr)
 
-            counter = 0
-            dl = DataLoader(self.buffer, batch_size=self.c.batch_size)
-            for tr in dl:
+            dl = DataLoader(self.buffer, batch_size=self.config.batch_size)
+            for i, tr in enumerate(dl):
                 observations, actions, rewards, states = map(lambda k: tr[k].transpose(0, 1).to(self.agent.device),
                                                              ('observations', 'actions', 'rewards', 'states'))
                 self.agent.learn(observations, actions, rewards, states)
-                counter += 1
-                if counter > self.c.training_steps:
+                if i > self.config.training_steps:
                     break
 
-            if t % 10 == 0:
-                res = []
-                for _ in range(5):
-                    tr = utils.simulate(self.env, policy, training=False)
-                    res.append(tr['rewards'].sum().item())
-                self.callback.add_scalar('eval/reward', mean(res), t*1000)
-            t += 1
+            if self.interactions_count % self.config.eval_freq == 0:
+                res = [utils.simulate(self.env, policy, training=False)['rewards'].sum() for _ in range(5)]
+                self.callback.add_scalar('eval/reward', np.mean(res), self.interactions_count)
+                self.callback.add_scalar('eval/std', np.std(res), self.interactions_count)
+
+    def save(self):
+        pass
+
+    def load(self, path):
+        pass
 
     def _make_env(self):
-        env = utils.make_env(self.c.task)
-        if self.c.encoder == 'MLP':
+        env = utils.make_env(self.config.task)
+        if self.config.observe == 'states':
             env = wrappers.dmWrapper(env)
+        elif self.config.observe == 'pixels':
+            env = wrappers.PixelsToGym(env)
+        elif self.config.observe == 'point_clouds':
+            env = wrappers.depthMapWrapper(env, device=self.config.device, points=self.config.pn_number, camera_id=0)
         else:
-            env = wrappers.depthMapWrapper(env, device=self.c.device, points=self.c.pn_number)
-        self.env = wrappers.FrameSkip(env, self.c.action_repeat)
-        sample_obs = self.env.reset()
-        act_dim = self.env.action_spec().shape[0]
-        self.encoder, self.decoder = utils.build_encoder_decoder(self.c, sample_obs.shape)
-        enc_obs = self.encoder(torch.from_numpy(sample_obs[None]))
-        return sample_obs.shape[0], act_dim, enc_obs.shape[1]
+            raise NotImplementedError
+        self.env = wrappers.FrameSkip(env, self.config.action_repeat)
+        act_dim = self.env.action_space.shape[0]
+        obs_dim = self.env.observation_space.shape[0]
+        return obs_dim, act_dim

src/models.py
@@ -1,33 +1,35 @@
 from .utils import build_mlp, TanhTransform
 import torch
+from torchvision import transforms as T
 nn = torch.nn
+F = nn.functional
 td = torch.distributions
 
 
 class DenseNormal(nn.Module):
-    def __init__(self, in_features, out_features, layers=[32, 32], min_std=1e-2):
+    def __init__(self, in_features, out_features, layers=(32, 32), min_std=1e-2):
         super().__init__()
-        self.fc = build_mlp([in_features]+layers+[2*out_features])
+        self.fc = build_mlp(in_features, *layers, 2*out_features)
         self.min_std = min_std
 
     def forward(self, x):
         mu, std = self.fc(x).chunk(2, -1)
         std = torch.maximum(std, torch.full_like(std, -18.))
-        std = nn.functional.softplus(std) + self.min_std
+        std = F.softplus(std) + self.min_std
         return mu, std
 
 
 class NormalActor(nn.Module):
     def __init__(self, obs_dim, act_dim, layers, mean_scale=5., init_std=5.):
         super().__init__()
-        self.model = build_mlp([obs_dim] + layers + [2*act_dim])
+        self.model = build_mlp(obs_dim, *layers, 2*act_dim)
         self.init_std = torch.log(torch.tensor(init_std).exp() - 1)
         self.mean_scale = mean_scale
 
     def forward(self, obs):
         mu, std = self.model(obs).chunk(2, -1)
         mu = self.mean_scale * torch.tanh(mu / self.mean_scale)
-        std = nn.functional.softplus(std + self.init_std) + 1e-3
+        std = F.softplus(std + self.init_std) + 1e-7
         dist = td.Normal(mu, std)
         dist = td.transformed_distribution.TransformedDistribution(dist, TanhTransform())
         return td.Independent(dist, 1)
@@ -100,49 +102,152 @@ class RSSM(nn.Module):
         dist = td.Normal(mu, std)
         return td.Independent(dist, 1)
 
-
-class PointCloudEncoder(nn.Module):
-    def __init__(self, in_features, depth, layers):
-        super().__init__()
-        coef = 2 ** (layers - 1)
-        self.model = nn.ModuleList([nn.Sequential(nn.Linear(in_features, coef*depth), nn.ReLU())])
-        for i in range(layers-1):
-            m = nn.Sequential(nn.Linear(coef*depth, coef // 2 * depth), nn.ReLU())
-            self.model.append(m)
-            coef //= 2
-
-        self.fc = nn.Sequential(
-            nn.Linear(depth, depth),
-            nn.Tanh(),
-        )
-
-    def forward(self, x):
-        for layer in self.model:
-            x = layer(x)
-        values, idx = torch.max(x, -2)
-        return self.fc(values)
-
+# class PointCloudEncoder(nn.Module):
+#     def __init__(self, in_features, depth, layers):
+#         super().__init__()
+#         coef = 2 ** (layers - 1)
+#         self.model = nn.ModuleList([nn.Sequential(nn.Linear(in_features, coef*depth), nn.ReLU())])
+#         for i in range(layers-1):
+#             m = nn.Sequential(nn.Linear(coef*depth, coef // 2 * depth), nn.ReLU())
+#             self.model.append(m)
+#             coef //= 2
+#
+#         self.fc = nn.Sequential(
+#             nn.Linear(depth, depth),
+#             nn.Tanh(),
+#         )
+#
+#     def forward(self, x):
+#         for layer in self.model:
+#             x = layer(x)
+#         values, idx = torch.max(x, -2)
+#         return self.fc(values)
+#
+#
+# class PointCloudDecoder(nn.Module):
+#     def __init__(self, in_features, out_features, depth, layers, pn_number):
+#         super().__init__()
+#
+#         self.coef = 2**(layers-1)
+#         self.fc = nn.Sequential(
+#             nn.Linear(in_features, self.coef * depth * pn_number),
+#             nn.ELU(inplace=True),
+#         )
+#
+#         self.deconvs = nn.ModuleList([nn.Unflatten(-1, (pn_number, self.coef * depth))])
+#         for _ in range(layers-1):
+#             self.deconvs.append(nn.Linear(self.coef * depth, self.coef * depth // 2))
+#             self.deconvs.append(nn.ELU(inplace=True))
+#             self.coef //= 2
+#
+#         self.deconvs.append(nn.Linear(depth, out_features))
+#
+#     def forward(self, x):
+#         x = self.fc(x)
+#         for layer in self.deconvs:
+#             x = layer(x)
+#         return x
 
 class PointCloudDecoder(nn.Module):
-    def __init__(self, in_features, out_features, depth, layers, pn_number):
+    def __init__(self, in_features, pn_number, depth=32, act=nn.ELU):
         super().__init__()
 
-        self.coef = 2**(layers-1)
-        self.fc = nn.Sequential(
-            nn.Linear(in_features, self.coef * depth * pn_number),
-            nn.ELU(inplace=True),
+        self.deconvs = nn.Sequential(
+            nn.Linear(in_features, 2*depth*pn_number),
+            act(),
+            nn.Unflatten(-1, (pn_number, 2*depth)),
+            nn.Linear(2*depth, depth),
+            act(),
+            nn.Linear(depth, 3)
         )
 
-        self.deconvs = nn.ModuleList([nn.Unflatten(-1, (pn_number, self.coef * depth))])
-        for _ in range(layers-1):
-            self.deconvs.append(nn.Linear(self.coef * depth, self.coef * depth // 2))
-            self.deconvs.append(nn.ELU(inplace=True))
-            self.coef //= 2
+    def forward(self, x):
+        return self.deconvs(x)
 
-        self.deconvs.append(nn.Linear(depth, out_features))
+
+class PointCloudEncoder(nn.Module):
+    def __init__(self, in_features, out_features, layers, dropout=0., act=nn.ELU):
+        super().__init__()
+        self.convs = nn.Sequential()
+
+        sizes = (in_features,) + layers
+        for i in range(len(sizes)-1):
+            block = nn.Sequential(
+                nn.Linear(sizes[i], sizes[i+1]),
+                act(),
+                nn.Dropout(dropout)
+            )
+            self.convs.add_module(f'conv{i}', block)
+
+        self.fc = nn.Sequential(
+            nn.Linear(sizes[-1], out_features),
+            nn.LayerNorm(out_features),
+            nn.Tanh()
+        )
+
+        self.soft = False
 
     def forward(self, x):
-        x = self.fc(x)
-        for layer in self.deconvs:
-            x = layer(x)
-        return x
+        x = self.convs(x)
+        # try soft sampling
+        if self.soft:
+            indices = F.gumbel_softmax(x, hard=True).argmax(-2)
+            values = torch.gather(x, -2, indices.unsqueeze(-2)).squeeze(-2)
+        else:
+            values, indices = torch.max(x, -2)
+        return self.fc(values), indices
+
+
+class PixelEncoder(nn.Module):
+    def __init__(self, in_channels=3, out_features=64, depth=32, act=nn.ELU):
+        super().__init__()
+
+        self.convs = nn.Sequential(
+            T.Normalize(.5, 1),
+            nn.Conv2d(in_channels, depth, 3, 2),
+            act(),
+            nn.Conv2d(depth, depth, 3, 1),
+            act(),
+            nn.Conv2d(depth, depth, 3, 1),
+            act(),
+            nn.Flatten(),
+            nn.Linear(depth*27*27, out_features),  # 37 if size = 84, 27 if 64
+            nn.LayerNorm(out_features),
+            nn.Tanh()
+        )
+
+    def forward(self, img):
+        reshape = img.ndimension() > 4  # hide temporal axis
+        if reshape:
+            seq_len, batch_size = img.shape[:2]
+            img = img.flatten(0, 1)
+        img = self.convs(img)
+        if reshape:
+            img = img.reshape(seq_len, batch_size, -1)
+        return img, None
+
+
+class PixelDecoder(nn.Module):
+    def __init__(self, in_features, out_channels=3, depth=32, act=nn.ELU):
+        super().__init__()
+        self.deconvs = nn.Sequential(
+            nn.Linear(in_features, depth*27*27),
+            act(),
+            nn.Unflatten(-1, (depth, 27, 27)),
+            nn.ConvTranspose2d(depth, depth, 3, 1),
+            act(),
+            nn.ConvTranspose2d(depth, depth, 3, 1),
+            act(),
+            nn.ConvTranspose2d(depth, out_channels, 3, 2, output_padding=1),
+            T.Normalize(-.5, 1)
+        )
+
+    def forward(self, x):
+        reshape = x.ndimension() > 2  # hide temporal axis
+        if reshape:
+            seq_len, batch_size = x.shape[:2]
+            x = x.flatten(0, 1)
+        img = self.deconvs(x)
+        if reshape:
+            img = img.reshape(seq_len, batch_size, 3, 64, 64)
+        return img

src/utils.py
@@ -6,13 +6,18 @@ import random
 from torch.utils.data import Dataset
 from dm_control import suite
 import math
+import dataclasses
+from abc import ABC
+from ruamel.yaml import YAML
 nn = torch.nn
 F = nn.functional
 td = torch.distributions
 
 
-def build_mlp(sizes, act=nn.ELU):
+def build_mlp(*sizes, act=nn.ELU):
     mlp = []
+    if len(sizes) == 1:
+        sizes = sizes[0]
     for i in range(1, len(sizes)):
         mlp.append(nn.Linear(sizes[i-1], sizes[i]))
         mlp.append(act())
@@ -36,29 +41,6 @@ def make_env(name, **kwargs):
     return suite.load(domain, task, **kwargs)
 
 
-def build_encoder_decoder(configs, obs_shape):
-    encoder = []
-    decoder = []
-    feat_dim = configs.stoch_dim + configs.deter_dim
-    if configs.encoder == 'MLP':
-        obs_dim = obs_shape[0]
-        encoder = [build_mlp([obs_dim, configs.emb_dim])]
-        decoder = [build_mlp([configs.deter_dim + configs.stoch_dim, obs_dim])]
-    elif configs.encoder == 'PointNet':
-        from .models import PointCloudDecoder, PointCloudEncoder
-        encoder.append(PointCloudEncoder(obs_shape[-1], configs.pn_depth, configs.pn_layers))
-        decoder.append(PointCloudDecoder(feat_dim, 3, configs.pn_depth, configs.pn_layers, obs_shape[0]))
-    elif configs.encoder == 'CNN':
-        from .models import PixelEncoder, PixelDecoder
-        enc = PixelEncoder(obs_shape[0], configs.cnn_depth, configs.cnn_layers)
-        _, _, width, height = enc.conv(torch.ones(1, *obs_shape)).shape
-        encoder.append(enc)
-        decoder.append(PixelDecoder(feat_dim, width, height, obs_shape[0], configs.cnn_depth, configs.cnn_layers))
-    else:
-        raise NotImplementedError
-    return nn.Sequential(*encoder), nn.Sequential(*decoder)
-
-
 def gve(rewards, values, discount, disclam):
     # seq_len, batch_size, _ = rewards.shape
     # assert values.shape == rewards.shape
@@ -75,11 +57,15 @@ def gve(rewards, values, discount, disclam):
 def gve2(rewards, values, discount, disclam):
     target_values = []
     last_val = values[-1]
-    for r, v in zip(rewards[:-1].flip(-1), values[1:].flip(-1)):
+    for r, v in zip(rewards[:-1].flip(0), values[1:].flip(0)):
         last_val = r + discount*(disclam*last_val + (1.-disclam)*v)
         target_values.append(last_val)
+    return torch.stack(target_values).flip(0)
 
-    return torch.stack(target_values).flip(-1)
+
+def soft_update(target, online, rho):
+    for pt, po in zip(target.parameters(), online.parameters()):
+        pt.copy_(rho * pt + (1. - rho) * po)
 
 
 def simulate(env, policy, training):
@@ -188,3 +174,17 @@ class PointCloudGenerator:
         depth = self.reshape_depth(depth)
         xyz = depth * self.uv1
         return xyz
+
+
+@dataclasses.dataclass
+class AbstractConfig(ABC):
+    def save(self, file_path):
+        yaml = YAML()
+        with open(file_path, 'w') as f:
+            yaml.dump(dataclasses.asdict(self), f)
+
+    def load(self, file_path):
+        yaml = YAML()
+        with open(file_path) as f:
+            config_dict = yaml.load(f)
+        return dataclasses.replace(self, **config_dict)

src/wrappers.py
@@ -5,17 +5,18 @@ import torch
 from .utils import PointCloudGenerator
 import ctypes
 from dm_control.mujoco.wrapper import MjvOption
+from dm_control.suite.wrappers import pixels
 
 
 class Wrapper:
     """ Partially solves problem with  compatibility"""
 
     def __init__(self, env):
-        self._env = env
-        self.observation_space, self.action_space = self._infer_spaces(env)
+        self.env = env
+        self._observation_space, self._action_space = self._infer_spaces(env)
 
     def observation(self, timestamp):
-        return np.float32(timestamp.observation)
+        return timestamp
 
     def reward(self, timestamp):
         return np.float32(timestamp.reward)
@@ -24,38 +25,46 @@ class Wrapper:
         return timestamp.last()
 
     def step(self, action):
-        timestamp = self._env.step(action)
+        timestamp = self.env.step(action)
         obs = self.observation(timestamp)
         r = self.reward(timestamp)
         d = self.done(timestamp)
         return obs, r, d, None
 
     def reset(self):
-        return self.observation(self._env.reset())
+        return self.observation(self.env.reset())
 
     @staticmethod
     def _infer_spaces(env):
-        lim = 1.
+        lim = float('inf')
         spec = env.action_spec()
         action_space = Box(low=spec.minimum.astype(np.float32), dtype=np.float32,
                            high=spec.maximum.astype(np.float32), shape=spec.shape)
         ar = list(env.observation_spec().values())[0]
+
         obs_sample = np.concatenate(list(map(lambda ar: ar.generate_value() if ar.shape != () else [1],
                                              env.observation_spec().values())))
-        obs_space = Box(low=-lim, high=lim, shape=obs_sample.shape, dtype=ar.dtype)
+
+        obs_space = Box(low=-lim, high=lim, shape=obs_sample.shape, dtype=np.float32)#ar.dtype)
         return obs_space, action_space
+
+    def __getattr__(self, item):
+        return getattr(self.env, item)
 
     @property
     def unwrapped(self):
-        if hasattr(self._env, 'unwrapped'):
-            return self._env.unwrapped
-        return self._env
+        env = self
+        while hasattr(env, 'env'):
+            env = env.env
+        return env
 
-    def __getattr__(self, item):
-        return getattr(self._env, item)
+    @property
+    def observation_space(self):
+        return self._observation_space
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}"
+    @property
+    def action_space(self):
+        return self._action_space
 
 
 class dmWrapper(Wrapper):
@@ -97,25 +106,19 @@ class depthMapWrapper(Wrapper):
                  points=1000,
                  ):
         super().__init__(env)
-        self._env = env
+        self.env = env
         self.points = points
         self._depth_kwargs = dict(camera_id=camera_id, height=height, width=width,
                                   depth=True, scene_option=self._prepare_scene())
         self.return_pos = return_pos
         self.pcg = PointCloudGenerator(**self.pc_params, device=device)
 
-    def reset(self):
-        return self.observation(self._env.reset())
-
-    def __getattr__(self, name):
-        return getattr(self._env, name)
-
     def observation(self, timestamp):
-        depth = self._env.physics.render(**self._depth_kwargs)
+        depth = self.env.physics.render(**self._depth_kwargs)
         pc = self.pcg.get_PC(depth)
         pc = self._segmentation(pc)
         if self.return_pos:
-            pos = self._env.physics.position()
+            pos = self.env.physics.position()
             return pc, pos
         return pc.detach().cpu().numpy()
 
@@ -134,15 +137,33 @@ class depthMapWrapper(Wrapper):
 
     def _prepare_scene(self):
         scene = MjvOption()
-        scene.ptr.contents.flags = (ctypes.c_uint8*22)(0)
+        scene.flags = (ctypes.c_uint8*22)(0)
 
         return scene
 
     @property
     def pc_params(self):
         # device
-        fovy = self._env.physics.model.cam_fovy[0]
+        fovy = self.env.physics.model.cam_fovy[0]
         return dict(
             camera_fovy=fovy,
-            image_height=self._depth_kwargs.get('height') or 240,
-            image_width=self._depth_kwargs.get('width') or 320)
+            image_height=self._depth_kwargs.get('height', 240),
+            image_width=self._depth_kwargs.get('width', 320)
+        )
+
+
+class PixelsToGym(Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.env = pixels.Wrapper(self.env, render_kwargs={'camera_id': 0, 'height': 64, 'width': 64})
+
+    def observation(self, timestamp):
+        obs = timestamp.observation['pixels']
+        obs = np.array(obs) / 255.
+        obs = np.array(obs)
+        return obs.transpose((2, 1, 0))
+
+    @property
+    def observation_space(self):
+        # correspondent space have to be extracted from the dm_control API -> gym API
+        return Box(low=0., high=1., shape=(64, 64, 3))